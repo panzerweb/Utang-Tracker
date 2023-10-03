4be33f991213e743cd05a2a64d271ac95f4bd90f
PanzerWebDeveloper <selwynvillar@gmail.com>
2023-10-03 21:19:42 +0800

    Update

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-//todo -  Initialize variables
+//Initialize variables
 let customer_name, product_name, quantity, price_number;
 
-//todo - total price get
+//initialize total price
 let totalPrice;
 
-//todo Get the debts
+// initialize debts as an empty array
 let debts = [];
 
+//Function to get value of initialized variables
 function initializeVariables(){
     customer_name = document.getElementById('customer-name').value;
     product_name = document.getElementById('product-name').value;
     quantity = document.getElementById('quantity-number').value;
     price_number = document.getElementById('price-number').value;
 
-    totalPrice = quantity * price_number;
+    totalPrice = quantity * price_number; //Multiply price and quantity
 }
 function addDebt(){
+    //Callback function
     initializeVariables();
 
+    //Add new Debts to push
     const testForPush = `${"Name: " + customer_name} | ${"Product: " + product_name} | ${"Quantity: " + quantity} | ${"Price: " + price_number}| ${"Total: " + totalPrice}`;
-    
+    //Finally push
     debts.push(testForPush);
-
+    //Run for testing
     console.log(debts);
 }
